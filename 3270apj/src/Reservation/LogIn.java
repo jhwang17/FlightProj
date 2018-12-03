e7e98d8b9b0452dc7e4f6c73ea5ab6b42ca68e53
peihan <peihan@10.238.174.150>
2018-12-02 22:12:57 -0500

    login - customer test

--- 3270apj/src/Reservation/LogIn.java
+++ 3270apj/src/Reservation/LogIn.java
@@ -5,7 +5,7 @@ import javafx.application.Application;
 import javafx.collections.ObservableList;
 import javafx.event.*;
 import javafx.geometry.*;
-import javafx.scene.Scene; 
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
@@ -16,28 +16,24 @@ import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import Data.CustomerData;
 import Application.Customer;
+import java.util.ArrayList;
+
 
 public class LogIn extends Application {
-	
-	Stage loginStage;
-	
 	public static void main(String[] args) {
-        launch(args);
-    }
+		launch(args);
+		}
 	
-	@Override 
 	public void start(Stage stage) {
-		loginStage = stage;
-		
 		GridPane gridPane = createLogInFormPane();
 		addUIControls(gridPane);
 		Scene scene = new Scene(gridPane, 300, 275);
 		stage.setScene(scene);
-		stage.setWidth(400);
-		stage.setHeight(400);
+		stage.setWidth(500);
+		stage.setHeight(500);
 		stage.setTitle("Welcome");
-        stage.show();   
-    }
+		stage.show();
+		}
 	
 	public GridPane createLogInFormPane() {
 		GridPane gridPane = new GridPane();
@@ -45,9 +41,8 @@ public class LogIn extends Application {
 		gridPane.setHgap(10);
 		gridPane.setVgap(10);
 		gridPane.setPadding(new Insets(25, 25, 25, 25));
-
 		return gridPane;
-	}
+		}	
 	
 	public void addUIControls(GridPane gridPane) {
 		//Header
@@ -65,7 +60,7 @@ public class LogIn extends Application {
 		Label passwordL = new Label("Password:");
 		gridPane.add(passwordL, 0, 2);
 		PasswordField passwordF = new PasswordField();
-		gridPane.add(passwordF, 1, 2);		
+		gridPane.add(passwordF, 1, 2);
 		
 		Button logInButton = new Button("Log in");
 		HBox h1 = new HBox();
@@ -85,79 +80,142 @@ public class LogIn extends Application {
 		h3.getChildren().add(registerButton);
 		gridPane.add(h3, 2, 4);
 		
+		
 		final Text actiontarget = new Text();
-        gridPane.add(actiontarget, 1, 6);
-        
+		gridPane.add(actiontarget, 1, 6);
+		/*
 		logInButton.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent event) {
 				actiontarget.setFill(Color.FIREBRICK);
-		    }
-		});
+		    }
+		});*/
 	
-        EventHandler<MouseEvent> loginEvent = new EventHandler<MouseEvent>() {
-        	@Override
-        	public void handle(MouseEvent e) {
-        		if(e.getButton() == MouseButton.PRIMARY) {
-        			try {
-        				if (userNameF.getText().isEmpty() || passwordF.getText().isEmpty()) {
-        	
-        					showAlert(Alert.AlertType.ERROR, gridPane.getScene().getWindow(), 
-        							"Login Error!", "Incorrect Username or Password");
-        					
-        				} else if (checkCridentials(userNameF.getText(),passwordF.getText())) {				
-        					Stage loginStage = new Stage();
-        					CustomerStage booking = new CustomerStage();
-        					booking.start(loginStage);
-        					loginStage.show();
-        					
-     
-        				} else if (userNameF.getText().equalsIgnoreCase("admin") && passwordF.getText().equalsIgnoreCase("admin")) {
-        					Stage loginStage = new Stage();
-        					AdminStage booking = new AdminStage();
-        					booking.start(loginStage);
-        					loginStage.show();
-        				}
-        			} catch (Exception ex) {
-    					ex.printStackTrace();
-        			}
-        			loginStage.close();
-        		}
-        	}
-        };
-               
-        logInButton.addEventHandler(MouseEvent.MOUSE_CLICKED, loginEvent);
-             
-        EventHandler<MouseEvent> registerEvent = new EventHandler<MouseEvent>() {
-    		@Override
-    		public void handle(MouseEvent e) {
-    			if(e.getButton() == MouseButton.PRIMARY) {
-    				try {
-    					Stage registerStage = new Stage();
-    					SignUpForm signUpForm = new SignUpForm();
-    					signUpForm.start(registerStage);
-    					registerStage.show();
-    				} catch(Exception ex) {
-    					ex.printStackTrace();
-    				}
-    			}
-    		}
-        };
-        		
-        registerButton.addEventHandler(MouseEvent.MOUSE_CLICKED, registerEvent);   		    
-	}
-	
-	boolean checkCridentials(String userName,String password) {
+		 EventHandler<MouseEvent> loginEvent = new EventHandler<MouseEvent>() {
+	         public void handle(MouseEvent e) {
+				 if(e.getButton() == MouseButton.PRIMARY) {
+					 if(checkCridentialsAdmin(userNameF.getText(),passwordF.getText())) {
+							try {
+								Stage adminStage = new Stage();
+								AdminStage booking = new AdminStage();
+								booking.start(adminStage);
+								adminStage.show();
+								}catch(Exception ex) {
+										
+								}
+					}
+					 else if(checkCridentialsUser(userNameF.getText(),passwordF.getText())) {
+						 try {
+        				 		Stage loginStage = new Stage();
+        				 		CustomerStage booking = new CustomerStage();
+        				 		booking.start(loginStage);
+        				 		loginStage.show();
+				
+        			 		}catch(Exception ex) {
+
+        			 		}
+					 }
+
+					 
+        		 
+			
+					 else {
+						 showAlert(Alert.AlertType.ERROR, gridPane.getScene().getWindow(), "Login Error!",
+														"Incorrect Username or Password");
+					 }
+				 }
+
+			 }
+		 };
+	         /* Annie	
+        				try {
+                				if (userNameF.getText().isEmpty() || passwordF.getText().isEmpty()) {
+                	
+                					showAlert(Alert.AlertType.ERROR, gridPane.getScene().getWindow(), 
+                							"Login Error!", "Incorrect Username or Password");
+                					
+                				} else if (checkCridentials(userNameF.getText(),passwordF.getText())) {				
+                					Stage loginStage = new Stage();
+                					Booking booking = new Booking();
+                					booking.start(loginStage);
+                					loginStage.show();
+             
+                				} else if (userNameF.getText().equalsIgnoreCase("admin") && passwordF.getText().equalsIgnoreCase("admin")) {
+                					Stage loginStage = new Stage();
+                					Booking booking = new Booking();
+                					booking.start(loginStage);
+                					loginStage.show();
+                				}
+                				}catch(Exception ex) {
+            						ex.printStackTrace();
+                			}	
+                		}
+                	}
+                };*/
+     logInButton.addEventHandler(MouseEvent.MOUSE_CLICKED, loginEvent);
+
+	EventHandler<MouseEvent> registerEvent = new EventHandler<MouseEvent>() {
+		@Override
+		public void handle(MouseEvent e) {
+			if(e.getButton() == MouseButton.PRIMARY) {
+				try {
+					Stage registerStage = new Stage();
+					SignUpForm signUpForm = new SignUpForm();
+					signUpForm.start(registerStage);
+					registerStage.show();
+					}catch(Exception ex) {
+
+					}
+			}
+		}
+	};
+	registerButton.addEventHandler(MouseEvent.MOUSE_CLICKED, registerEvent);
+		 }
+		
+
+	boolean checkCridentialsUser(String userName,String password) {
 		ObservableList<Customer> customers = CustomerData.getCustomer();
+
+		if(customers.isEmpty()) {
+			return false;		}
+		else {
 			for(Customer c:customers) {
-				if (c.getUserName().equals(userName) && c.getPassword().equals(password)) {
-						return true;					
+				if(c.getUserName() == (userName) ) {
+					if(password.equals(c.getPassword())) {
+						return true;
+					}
+					else {
+						return false;
+					}
 				}
 			}
 			return false;
 		}
-        
-	public void showAlert(Alert.AlertType alertType, Window owner, String title, String message) {
+	}
+	boolean checkCridentialsAdmin(String userName,String password){
+				if(userName.equalsIgnoreCase("Admin") ) {
+					if(password.equalsIgnoreCase("Admin")) {
+						return true;
+					}
+					else {
+						return false;
+					}
+				}
+			return false;
+
+	}
+	
+	
+	
+		/*Annie
+		for(Customer c:customers) {
+			if (c.getUserName().equals(userName) && c.getPassword().equals(password)) {
+					return true;					
+			}
+		}
+		return false;
+	}*/
+	public void showAlert(Alert.AlertType alertType,Window owner,String title,String message) {
 		Alert alert = new Alert(alertType);
 		alert.setTitle(title);
 		alert.setHeaderText(null);
@@ -165,5 +223,5 @@ public class LogIn extends Application {
 		alert.initOwner(owner);
 		alert.show();
 	}
-        
 }
+
